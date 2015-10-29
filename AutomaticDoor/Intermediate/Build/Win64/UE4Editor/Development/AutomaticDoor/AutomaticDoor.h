40b0beda4c42d1ad764ff948db4dee110414a2bf
He-Man <svenssonsnabel@gmail.com>
2015-10-29 22:39:32 +0100

    HealthBar, GameHUD (Fungerar inte riktigt)

--- AutomaticDoor/Intermediate/Build/Win64/UE4Editor/Development/AutomaticDoor/AutomaticDoor.h
+++ AutomaticDoor/Intermediate/Build/Win64/UE4Editor/Development/AutomaticDoor/AutomaticDoor.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_AUTOMATICDOOR_H__
 #define __AUTO_AUTOMATICDOOR_H__
-//Last Write: 2015-10-29 19:46:07 +00:00
+//Last Write: 2015-10-29 20:16:02 +00:00
 #include "C:\Users\snabel\Documents\GitHub\UnrealTutorial\AutomaticDoor\Source\AutomaticDoor\AutomaticDoor.h"
 #endif//__AUTO_AUTOMATICDOOR_H__
