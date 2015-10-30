39e3041044df36d666ea9708827ada8de77d7613
He-Man <svenssonsnabel@gmail.com>
2015-10-30 11:57:44 +0100

    Fungerande HealthBar och en StartMenu med knapp

--- AutomaticDoor/Intermediate/Build/Win64/UE4Editor/Development/AutomaticDoor/AutomaticDoor.h
+++ AutomaticDoor/Intermediate/Build/Win64/UE4Editor/Development/AutomaticDoor/AutomaticDoor.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_AUTOMATICDOOR_H__
 #define __AUTO_AUTOMATICDOOR_H__
-//Last Write: 2015-10-29 20:16:02 +00:00
-#include "C:\Users\snabel\Documents\GitHub\UnrealTutorial\AutomaticDoor\Source\AutomaticDoor\AutomaticDoor.h"
+//Last Write: 2015-10-30 10:01:33 +00:00
+#include "C:\Users\Spellabbet\Documents\GitHub\UnrealTutorial\AutomaticDoor\Source\AutomaticDoor\AutomaticDoor.h"
 #endif//__AUTO_AUTOMATICDOOR_H__
